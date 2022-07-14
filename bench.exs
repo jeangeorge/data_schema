5d456135833a296d5cc4d0d3d188dda875f57fcd
Jean George <jean.george.alves@gmail.com>
2022-07-14 16:23:07 -0300

    new tests

--- bench.exs
+++ bench.exs
@@ -1,16 +1,6 @@
 :erts_internal.debug_on()
 
-large_xml = File.read!(Path.expand("random.xml"))
+large_xml = File.read!(Path.expand("really_large_xml_fixture.xml"))
 {:ok, x} = DataSchema.Saxy.StructHandler.parse_string(large_xml);
-
-Benchee.run(
-  %{
-    "StructHanlder" => fn ->
-      DataSchema.to_struct(x, AirShop)
-    end
-  },
-  memory_time: 1,
-  reduction_time: 1
-)
 
 :erts_internal.debug_off()
