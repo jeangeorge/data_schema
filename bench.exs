0b87ae956c39cb9c7ba0e143eb541de7af190bac
Jean George <jean.george.alves@gmail.com>
2022-07-14 16:27:26 -0300

    new tests

--- bench.exs
+++ bench.exs
@@ -1,6 +1,6 @@
 :erts_internal.debug_on()
 
 large_xml = File.read!(Path.expand("really_large_xml_fixture.xml"))
-{:ok, x} = DataSchema.Saxy.StructHandler.parse_string(large_xml);
+{:ok, _} = DataSchema.Saxy.StructHandler.parse_string(large_xml);
 
 :erts_internal.debug_off()
