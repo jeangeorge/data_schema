fe0db90a87f9a40bfc9cba7e7c87910cd31b079a
Jean George <jean.george.alves@gmail.com>
2022-07-14 16:15:35 -0300

    Revert "new tests"
    This reverts commit 61ce389b29d46c7a5d8c18fec5e6f3b231f2fd17.

--- bench.exs
+++ bench.exs
@@ -1,6 +1,16 @@
 :erts_internal.debug_on()
 
 large_xml = File.read!(Path.expand("random.xml"))
-{:ok, _} = DataSchema.Saxy.StructHandler.parse_string(large_xml);
+{:ok, x} = DataSchema.Saxy.StructHandler.parse_string(large_xml);
+
+Benchee.run(
+  %{
+    "StructHanlder" => fn ->
+      DataSchema.to_struct(x, AirShop)
+    end
+  },
+  memory_time: 1,
+  reduction_time: 1
+)
 
 :erts_internal.debug_off()
